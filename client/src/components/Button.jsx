002c15d9eb7b0eb133cd76219c88a828277d6932
ansariafzal <ansariafzal2112@gmail.com>
2024-07-19 01:01:58 +0530

    hover

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ children, onClick, className }) => {
     <button
       type="button"
       onClick={onClick}
-      className={`px-4 py-3 bg-violet-500 text-white font-medium rounded-lg ${className}`}
+      className={`px-4 py-3 bg-violet-600 text-white font-medium rounded-lg hover:bg-violet-500 ${className}`}
     >
       {children}
     </button>
